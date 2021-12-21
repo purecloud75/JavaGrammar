0478385dccb3081851d4662533bf875efdb1655b
purecloud75 <purecloud75@gmail.com>
2021-12-21 23:29:49 +0900

    Field Constructor

--- src/chap6/FandC/Car.java
+++ src/chap6/FandC/Car.java
@@ -7,7 +7,7 @@ public class Car {
     int maxSpeed = 350;
     int speed;
 
-    Car() {}  //디폴트 생성자로, 라이브러리 클래스에서 별도로 생성자함수를 만들지 않으면, 컴파일러가 자동으로 만들어주는 생성자.
+    public Car() {}  //디폴트 생성자로, 라이브러리 클래스에서 별도로 생성자함수를 만들지 않으면, 컴파일러가 자동으로 만들어주는 생성자.
     Car(String model) {
         this.model = model;
     }
