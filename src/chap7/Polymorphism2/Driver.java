3edbd92ca5acd6a44be035b51e4ec9060d7ff9e0
purecloud75 <purecloud75@gmail.com>
2021-12-25 11:59:47 +0900

    다형성

---
+++ src/chap7/Polymorphism2/Driver.java
@@ -0,0 +1,8 @@
+package chap7.Polymorphism2;
+
+public class Driver {
+
+    public void drive(Vehicle vehicle) {
+        vehicle.run();
+    }
+}
---
+++ src/chap7/Polymorphism2/Taxi.java
@@ -0,0 +1,9 @@
+package chap7.Polymorphism2;
+
+public class Taxi extends Vehicle{
+
+    @Override
+    public void run() {
+        System.out.println("택시가 달립니다.");
+    }
+}
