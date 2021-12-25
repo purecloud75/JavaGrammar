00f094ac25a9fbcfcdfae670a9ddcd964a428992
purecloud75 <purecloud75@gmail.com>
2021-12-25 11:59:04 +0900

    상속

---
+++ src/chap7/Inheritance/cellPhone.java
@@ -0,0 +1,36 @@
+package chap7.Inheritance;
+
+public class cellPhone {
+
+    String model;
+    String color;
+
+    cellPhone() {}
+    cellPhone(String model) {
+        this.model = model;
+    }
+
+    void powerOn() {
+        System.out.println("전원 켜주세요");
+    }
+
+    void powerOff() {
+        System.out.println("전원 꺼주세요");
+    }
+
+    void bell() {
+        System.out.println("띠리링 띠리링");
+    }
+
+    void sendVoice(String message) {
+        System.out.println("자기: " + message);
+    }
+
+    void receiveVoice(String message) {
+        System.out.println("상대방: " + message);
+    }
+
+    void hangUp() {
+        System.out.println("전화를 끊습니다.");
+    }
+}
