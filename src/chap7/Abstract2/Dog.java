e71b0123f620b2bbe77e6fefe91a500c632f9c86
purecloud75 <purecloud75@gmail.com>
2021-12-26 12:31:27 +0900

    추상 클래스

---
+++ src/chap7/Abstract2/Dog.java
@@ -0,0 +1,13 @@
+package chap7.Abstract2;
+
+public class Dog extends Animal {
+
+    public Dog() {
+        this.kind = "포유류";
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("멍멍");
+    }
+}
