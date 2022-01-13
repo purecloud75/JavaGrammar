ed10c1e43296d4a320a5a2f64f24b52e78e66f7e
purecloud75 <purecloud75@gmail.com>
2022-01-13 22:45:12 +0900

    test

--- src/chap6/Test1/Person.java
+++ src/chap6/Test1/Person.java
@@ -18,6 +18,10 @@ public class Person {
         this.age = age;
     }
 
+    public Person getPerson() {
+        return this;
+    }
+
     //method
     void printAll() {
         System.out.println("키가 "+height+"이고 몸무게가 "+weight+"킬로인 "+sex+"이 있습니다. 이름은 "+name+"이고 나이는"+age);
--- src/chap6/Test1/Test.java
+++ src/chap6/Test1/Test.java
@@ -8,6 +8,10 @@ public class Test {
 
         Person person2 = new Person(178,80,"남성","Tomas",37);
         person2.printAll();
+        System.out.println(person2);
+        Person cho = person2.getPerson();
+        //어떤 객체에서 이 메소드를 호출하면, 해당 객체 자신의 주소값을 반환해주는 데에 this 가 쓰인다.
+        System.out.println(cho);
 
         Order order1 = new Order();
         order1.setOrder(12345678,202201120003L,"서울시 어딘가",0003);
